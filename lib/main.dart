02cf913702b51ebfd6044300f563b031b4704bfb
Ishan Dongol <ishandongol@gmail.com>
2022-07-25 23:17:56 +0545

    Custom Font and Appbar Color

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Plant Disease',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +22,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        appBarTheme: AppBarTheme(
+            titleTextStyle: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold, fontFamily: 'Poppins')),
+        fontFamily: "Poppins",
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Dashboard'),
     );
   }
 }
@@ -73,7 +79,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(
+            child: Text(
+          widget.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(color: Colors.black),
+        )),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -105,11 +118,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
